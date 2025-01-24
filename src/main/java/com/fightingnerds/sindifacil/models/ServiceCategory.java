99d6d0e85ab6e988254e52e87f3f9df93901589b
Said Rodrigues <coderflemis@gmail.com>
2025-01-24 14:06:41 -0300

    feat: add ServiceCategory

---
+++ src/main/java/com/fightingnerds/sindifacil/models/ServiceCategory.java
@@ -0,0 +1,15 @@
+package com.fightingnerds.sindifacil.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ServiceCategory extends DateAudit {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+}
