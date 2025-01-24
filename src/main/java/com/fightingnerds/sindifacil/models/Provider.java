b1b442ef930e35b9dc4a22769468c21cdf3a460b
Said Rodrigues <coderflemis@gmail.com>
2025-01-24 14:06:19 -0300

    feat: add Provider

---
+++ src/main/java/com/fightingnerds/sindifacil/models/Provider.java
@@ -0,0 +1,17 @@
+package com.fightingnerds.sindifacil.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Provider extends DateAudit {
+	@Id
+	@Column(name = "provider_id")
+	private String id;
+
+	@MapsId
+	@OneToOne(cascade = CascadeType.ALL)
+	private User user;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	private Address officeAddress;
+}
