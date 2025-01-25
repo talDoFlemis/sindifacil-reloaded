e583208d6155ff23b7991dfe6a5a71a566339b6b
Said Rodrigues <coderflemis@gmail.com>
2025-01-25 07:59:41 -0300

    feat: add service provider to provider controller

--- src/main/java/com/fightingnerds/sindifacil/controllers/ProviderController.java
+++ src/main/java/com/fightingnerds/sindifacil/controllers/ProviderController.java
@@ -1,7 +1,10 @@
 package com.fightingnerds.sindifacil.controllers;
 
 import com.fightingnerds.sindifacil.forms.ProviderForm;
+import com.fightingnerds.sindifacil.forms.ServiceProviderForm;
 import com.fightingnerds.sindifacil.models.Provider;
+import com.fightingnerds.sindifacil.models.Service;
+import com.fightingnerds.sindifacil.models.ServiceProvider;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -16,5 +19,14 @@ public class ProviderController {
 	}
 
 	public void editProvider(Provider provider, ProviderForm providerForm) {
+	}
+
+	public void addServiceToProvider(Provider provider, Service service, ServiceProviderForm serviceProviderForm) {
+	}
+
+	public void removeServiceFromProvider(ServiceProvider serviceProvider) {
+	}
+
+	public void editServiceForProvider(ServiceProvider serviceProvider, ServiceProviderForm serviceProviderForm) {
 	}
 }
