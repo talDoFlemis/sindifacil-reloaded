a3d28c8499cb61f568e6ba148ab1e411f33fc2e2
Said Rodrigues <coderflemis@gmail.com>
2025-02-01 22:15:32 -0300

    refactor(spring): remove white label error

--- src/main/java/com/fightingnerds/sindifacil/SindifacilApplication.java
+++ src/main/java/com/fightingnerds/sindifacil/SindifacilApplication.java
@@ -2,8 +2,9 @@ package com.fightingnerds.sindifacil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class SindifacilApplication {
 
 	public static void main(String[] args) {
