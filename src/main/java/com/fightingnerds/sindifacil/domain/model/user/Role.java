78f2cd1f96c4fc12157ea5327209e216bcfde264
Said Rodrigues <coderflemis@gmail.com>
2025-02-02 11:21:36 -0300

    refactor(Role): add constant values

--- src/main/java/com/fightingnerds/sindifacil/domain/model/user/Role.java
+++ src/main/java/com/fightingnerds/sindifacil/domain/model/user/Role.java
@@ -1,7 +1,21 @@
 package com.fightingnerds.sindifacil.domain.model.user;
 
+import lombok.experimental.FieldNameConstants;
+
+@FieldNameConstants(onlyExplicitlyIncluded = true)
 public enum Role {
-	SYNDIC,
-	JOINT_OWNER,
-	MANAGER;
+	@FieldNameConstants.Include SYNDIC("SYNDIC"),
+	@FieldNameConstants.Include JOINT_OWNER("JOINT OWNER"),
+	@FieldNameConstants.Include MANAGER("MANAGER");
+
+	private final String displayName;
+
+	Role(String displayName) {
+		this.displayName = displayName;
+	}
+
+	@Override
+	public String toString() {
+		return displayName;
+	}
 }
