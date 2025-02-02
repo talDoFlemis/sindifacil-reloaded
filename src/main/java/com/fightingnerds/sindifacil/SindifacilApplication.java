76fd027112fc0496941f83f3fffa3f95309034a4
Said Rodrigues <coderflemis@gmail.com>
2025-02-02 09:58:40 -0300

    feat: add configuration properties scan to application

--- src/main/java/com/fightingnerds/sindifacil/SindifacilApplication.java
+++ src/main/java/com/fightingnerds/sindifacil/SindifacilApplication.java
@@ -3,8 +3,10 @@ package com.fightingnerds.sindifacil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
+@ConfigurationPropertiesScan
 public class SindifacilApplication {
 
 	public static void main(String[] args) {
