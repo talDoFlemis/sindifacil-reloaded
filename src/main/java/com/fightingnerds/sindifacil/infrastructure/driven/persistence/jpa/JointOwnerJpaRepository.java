15973943b6c7eb799dba4cdd85d193f484ee9557
Said Rodrigues <coderflemis@gmail.com>
2025-02-11 11:16:30 -0300

    fix: joint owner jpa repository not using entity

--- src/main/java/com/fightingnerds/sindifacil/infrastructure/driven/persistence/jpa/JointOwnerJpaRepository.java
+++ src/main/java/com/fightingnerds/sindifacil/infrastructure/driven/persistence/jpa/JointOwnerJpaRepository.java
@@ -1,9 +1,9 @@
 package com.fightingnerds.sindifacil.infrastructure.driven.persistence.jpa;
 
-import com.fightingnerds.sindifacil.domain.model.user.JointOwner;
+import com.fightingnerds.sindifacil.infrastructure.driven.persistence.entity.JointOwnerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface JointOwnerJpaRepository extends JpaRepository<JointOwner, UUID> {
+public interface JointOwnerJpaRepository extends JpaRepository<JointOwnerEntity, UUID> {
 }
