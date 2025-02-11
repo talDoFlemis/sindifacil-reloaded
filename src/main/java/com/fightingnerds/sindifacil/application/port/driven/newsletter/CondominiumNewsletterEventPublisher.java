b7555f8c240e59d74701fa284635d7d84ba780fe
Said Rodrigues <coderflemis@gmail.com>
2025-02-11 08:47:07 -0300

    chore: inject event manager on publishers

--- src/main/java/com/fightingnerds/sindifacil/application/port/driven/newsletter/CondominiumNewsletterEventPublisher.java
+++ src/main/java/com/fightingnerds/sindifacil/application/port/driven/newsletter/CondominiumNewsletterEventPublisher.java
@@ -1,11 +1,12 @@
 package com.fightingnerds.sindifacil.application.port.driven.newsletter;
 
+import com.fightingnerds.sindifacil.application.port.driven.EventManager;
 import com.fightingnerds.sindifacil.application.port.driven.EventPublisher;
 import com.fightingnerds.sindifacil.domain.event.newsletter.CondominiumNewsletterEvent;
 import org.springframework.context.ApplicationEventPublisher;
 
 public abstract class CondominiumNewsletterEventPublisher extends EventPublisher<CondominiumNewsletterEvent> {
-	protected CondominiumNewsletterEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
-		super(applicationEventPublisher);
+	protected CondominiumNewsletterEventPublisher(EventManager eventManager) {
+		super(eventManager);
 	}
 }
--- src/main/java/com/fightingnerds/sindifacil/application/port/driven/newsletter/GlobalNewsletterEventPublisher.java
+++ src/main/java/com/fightingnerds/sindifacil/application/port/driven/newsletter/GlobalNewsletterEventPublisher.java
@@ -1,11 +1,12 @@
 package com.fightingnerds.sindifacil.application.port.driven.newsletter;
 
+import com.fightingnerds.sindifacil.application.port.driven.EventManager;
 import com.fightingnerds.sindifacil.application.port.driven.EventPublisher;
 import com.fightingnerds.sindifacil.domain.event.newsletter.GlobalNewsletterEvent;
 import org.springframework.context.ApplicationEventPublisher;
 
 public abstract class GlobalNewsletterEventPublisher extends EventPublisher<GlobalNewsletterEvent> {
-	protected GlobalNewsletterEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
-		super(applicationEventPublisher);
+	protected GlobalNewsletterEventPublisher(EventManager eventManager) {
+		super(eventManager);
 	}
 }
